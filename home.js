7c92f07168cfe83c78887c5df51cc519ce3d0d21
Muddapu-SriLatha <155659926+Muddapu-SriLatha@users.noreply.github.com>
2024-04-05 10:45:05 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,9 @@
-
+function login(){
+    document.location='login.html' ;
+}
+function signup(){
+    document.location='signup.html';
+}
+function gamepage(){
+    document.location='game.html';
+}
